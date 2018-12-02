baafe7bcf02ff0209abc45f17c8d4c376c011e1d
Fran R <franchy012@gmail.com>
2018-12-02 18:46:51 -0300

    Primer Commit

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'sinceryl@gmail.com'
+},
+{
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Errt',
+    email: 'erwinhl@gmail.com'
+},
+{
+    id: 3,
+    name: 'Clementine Fox',
+    username: 'Foxcle',
+    email: 'foxclementine@gmail.com'
+},
+{
+    id: 4,
+    name: 'Patricia Leck',
+    username: 'PatoL',
+    email: 'LeckPato@gmail.com'
+},
+{
+    id: 5,
+    name: 'Patrick Deltor',
+    username: 'PatDel',
+    email: 'pdeltor@gmail.com'
+},
+{
+    id: 6,
+    name: 'Dennis Scheld',
+    username: 'Scheldenis',
+    email: 'scheld_denis_11@gmail.com'
+},
+{
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwin',
+    email: 'kurtis.weis.12@gmail.com'
+},
+{
+    id: 8,
+    name: 'Nicholas Spark',
+    username: 'Nacho',
+    email: 'NikkoS@gmail.com'
+},
+{
+    id: 9,
+    name: 'Martin Rich',
+    username: 'TinchoR',
+    email: 'MatinchoR@gmail.com'
+},
+{
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'ClemenDu',
+    email: 'dubuqueC.21@gmail.com'
+},
+]
